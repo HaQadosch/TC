ebdacd2f21d012da2eef7f5f5713db1440db5c94
HaQadosch <haqadosch@gmail.com>
2014-12-16 16:22:51 +0000

    First UPload UKTC_IntentMedia

---
+++ UKTC_IntentMedia.js
@@ -0,0 +1,4 @@
+// Package Path - Search Results Page: tck_ssn_pkg_lst_ads_uk
+// Package Path - Details Page: tck_ssn_pkg_det_ads_uk
+// Hotels Path - Search Results Page: tck_ssn_hot_lst_ads_uk
+// Hotels Path - Details Page: tck_ssn_hot_det_ads_uk
--- draft.js
+++ draft.js
@@ -1,49 +1,44 @@
-<script>
-(function gtm_gatcSRP(jQ, cdl, gadl, w, d){
-    'use strict';
-    if (jQ && cdl && gadl) try {
-        var tn = gadl.trackername+'.' || ''
-        var gawa = gadl.webanalytics || {}
-        w._gaq = w._gaq || []
-        var wgp = function gtm_wgp(arr){ return w._gaq.push(arr)};
+<script id='gtm_Trivago'>
+(function gtm_TrivagoDL(cdl, dl) {
+    'use strict'
+    if (cdl && dl) try {
+        var cdpm = cdl.CATTParams
 
-        wgp([tn+'_setAccount', gadl.profileid]);
-        wgp([tn+'_setDomainName', gadl.cookiedomain]);
-        wgp([tn+'_setAllowLinker', gadl.allowlinker]);
-        wgp([tn+'_addIgnoredRef', gadl.addignoredref]);
-
-        if (jQ && gadl.organicsearch) jQ.each(gadl.organicsearch, function gtm_organicSearch(domain, term){wgp([tn+'_addOrganic', domain, term])})
-        if (jQ && gawa.customvars) jQ.each(gawa.customvars, function gtm_customvars(_, custVar){if (custVar.value !== '') wgp([tn+'_setCustomVar', custVar.index, custVar.name, custVar.value, custVar.scope])});
-
-        var fullVP = gawa.vp+'?'+gawa.qsp_st+'&'+gawa.qsp_cat+'&'+gawa.qsp_p;
-        wgp([tn+'_trackPageview', fullVP])
-
-        for (evt in gawa.events) {
-            var gevt = gawa.events[evt]
-            if (gevt.action) wgp([tn+'_trackEvent', gevt.category, gevt.action, gevt.label, gevt.value, gevt.noninteraction]);
-        };
-
-        (function gtm_callGAscript() {var ga = d.createElement('script'); ga.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js'; var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s)}());
-
-        var accoms = jQ('.js-result-item') || jQ([]);
-        accoms.find('button.src-srpResult-headerWishlist').on('click', function(d){
-            wgp([tn+'_trackEvent', 'FrogWishList', d.target.innerText.replace(/Wish List/i, 'Add'), d.target.parentElement.parentElement.dataset.resultId || 0])
-        });
-
-        try{
-            if (/lastselfacet=/i.test(location.search)){
-                var facetID =  /lastselfacet=([^&\s]+)/i.exec(location.search) && /lastselfacet=([^&\s]+)/i.exec(location.search).pop() || 0
-                var facets=[];facets[12004]="Star Rating";facets[18002]="TripAdvisor Rating";facets[17002]="Facilities";facets[56008]="Activities";facets[17005]="Location";facets[99010]="Discount in percentage";facets[99018]="Discount in pounds";facets[99012]="Tour Operator";facets[65012]="Product";facets[19012]="Party";facets[19008]="Departure Date";facets[19010]="Holiday Duration";facets[19006]="Departure Airport";facets[62010]="Destination";facets[30001]="Resort";facets[19002]="Board Basis";facets[87012]="Featured Holiday";facets[86010]="Price Per Adult"
-                wgp([tn+'_trackEvent', 'ATG Facet Interaction', 'Facet Selected', facets[facetID] || ('unknown_'+facetID), 1, true]);
-            } else if (/clear=/i.test(location.search)){
-                var clearedFacet =  /clear=([^&\s]+)/i.exec(location.search) && /clear=([^&\s]+)/i.exec(location.search).pop() || 'unknown'
-                wgp([tn+'_trackEvent', 'ATG Facet Interaction', 'Clear Facet', clearedFacet, 1, true]);
+        cdl.DL_trivago = {
+            partnerid    : 91,
+            hotel         : (cdpm.accomcode || '').replace(/(\w+)\|(.*)/g, '$2_$1'),
+            arrival     : +new Date(cdpm.deptdate && cdpm.deptdate.replace(/(\d\d)\/(\d\d)\/(\d\d\d\d)/, '$3 $2 $1') ||  0) || 0,
+            departure     : +new Date(cdpm.retdate && cdpm.retdate.replace(/(\d\d)\/(\d\d)\/(\d\d\d\d)/, '$3 $2 $1') || 0) || 0,
+            price         : cdpm.bookingvalue || 0,
+            currency     : cdpm.currency || 'GBP',
+            pixel         : {
+                src     : 'https://secde.trivago.com/page_check.php?pagetype=track&ref=_partnerid_&hotel=_hotel_&arrival=_arrival_&departure=_departure_&volume=_price_&currency=_currency_',
+                status     : 'not fired'
             }
-        } catch(e) {}
-
-    } catch(e) {
-        cdl.error('GTM UK TC GATC SRP: '+e)
+        }
+    } catch(e){
+        cdl.error("GTM Trivago: "+e)
+    } finally {
+        dl.push({event: 'DL Trivago'});
     }
-    return jQ && cdl && gadl
-}(window.jQuery, window.CATTDL, !window.CATTDL?!1:window.CATTDL.DL_gatc, window, document))
+    return cdl && dl && cdl.DL_trivago
+}(window.CATTDL, window.dataLayer));
+
+(function gtm_Trivago(jQ, cdl, tgdl) {
+    'use strict';
+    if (jQ && cdl && tgdl) try {
+        var img = jQ([])
+        var src = tgdl.pixel && tgdl.pixel.src.replace(/_partnerid_/, tgdl.partnerid).replace(/_hotel_/, tgdl.hotel).replace(/_arrival_/, tgdl.arrival).replace(/_departure_/, tgdl.departure).replace(/_price_/, tgdl.price).replace(/_currency_/, tgdl.currency) || ''
+        if (src) {
+            img = jQ('<img width="1" height=1" style="border-style:none;" alt="" id="TrivagoPixel">');
+            img.attr('src', src);
+            img.appendTo('body');
+            tgdl.pixel.src = src;
+            tgdl.pixel.status = 'fired'
+        }
+    } catch(e) {
+        cdl.error('GTM Trivago: '+e)
+    }
+    return jQ && cdl && tgdl
+}(window.jQuery, window.CATTDL, !window.CATTDL?!1:window.CATTDL.DL_trivago))
 </script>
