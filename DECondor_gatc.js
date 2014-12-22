906da685a5d6fb477bb52a7785a16d5c36ac70f9
HaQadosch <haqadosch@gmail.com>
2014-12-22 18:29:41 +0000

    using CATTDL for parameters

---
+++ DECondor_gatc.js
@@ -0,0 +1,77 @@
+<script>
+(function gtm_gatc(cdl, cdpm) {
+    'use strict'
+    window._gaq = window._gaq || [];
+    window._gaq.push(['_require', 'inpage_linkid', '//www.google-analytics.com/plugins/ga/inpage_linkid.js']);
+    window._gaq.push(['CATTGATC._setAccount', 'UA-40916028-1']);
+
+    var domainName = location.host.match(/fliegenpreise/gi)?'fliegenpreise.de':(location.host.match(/catchafly-t/gi)?'catchafly-t.com':(location.host.match(/vuelayvolaras/gi)?'vuelayvolaras.com':'condor.com'))
+    window._gaq.push(['CATTGATC._setDomainName', domainName]);
+    window._gaq.push(['CATTGATC._setAllowLinker', true]);
+    window._gaq.push(['_gat._anonymizeIp']);
+    window._gaq.push(['CATTGATC._addOrganic', 'suche.t-online.de', 'q'], ['CATTGATC._addOrganic', 'suche.web.de', 'q'], ['CATTGATC._addOrganic', 'suche.gmx.net', 'q'], ['CATTGATC._addOrganic', 'search.1und1.de', 'q'], ['CATTGATC._addOrganic', 'suche.freenet.de', 'query']);
+
+    if (cdl && cdpm) try {
+        var  QSP_ST = QSP_CAT = QSP_P = '';
+        var contentCATT = '';
+        for (var i in cdpm) {
+            contentCATT = cdpm[i];
+            if (contentCATT !== "") QSP_P += '&'+i+'='+contentCATT
+        }
+        QSP_P = QSP_P.replace(/^\&/, '').replace(/\=$/, '');
+
+        if (/holidays/i.test(location.host || document.URL)) {
+            if (/group/i.test(location.pathname || document.URL)) {
+                QSP_ST = 'SS_ST='+'|departure|destination|date-departuredate|search-durationchoice|inputDuration|traveller'+'&'; // $('input[type=hidden]')
+                QSP_CAT = 'SS_CAT=holidays_group_de&';
+            } else if (/offer/i.test(location.pathname || document.URL)) {
+                QSP_ST = 'SS_ST='+'|departure|destination|date-departuredate|search-durationchoice|inputDuration|traveller'+'&';
+                QSP_CAT = 'SS_CAT=holidays_offer_de&';
+            }
+        } else if ((/search|select/i).test(cdpm.pageid) && (cdpm.departureairportsearched != "")) {
+            QSP_ST = 'SS_ST='+(cdpm.departureairportsearched || '')+ '-'+(cdpm.returndestinationairportsearched || '')+ '|'+(cdpm.destinationairportsearched || '')+ '-'+(cdpm.returndepartureairportsearched || '')+'|'+(cdpm.deptdate || '')+ '|'+(cdpm.flighttype || '')+ '|'+(cdpm.durationsearched || '')+ '|'+(cdpm.paxadult || '')+ '|'+(cdpm.paxchild || '')+ '|'+(cdpm.paxinfant || '')+'&';
+            QSP_CAT = 'SS_CAT='+(cdpm.holidaytype || '')+'_'+(cdpm.pageid || '')+'_'+(cdpm.language || '')+'&';
+        };
+
+        var fullVP = '/VP'+(/mcfi/i.test(cdpm.pageContext)?"M":"")+'/'+(cdpm.language == ""?"":cdpm.language+"/")+(cdpm.lob || '')+'/'+(cdpm.holidaytype || '')+'/'+(cdpm.pageid || '')+'?'+QSP_ST+QSP_CAT+QSP_P;
+        fullVP = fullVP.toLowerCase();
+
+        if (/intcmp/i.test(document.location.href)){
+            var campaignID = ((/intcmp=([^&|\s|\?]*)/i).exec(document.location.href) || []).pop() || '';
+            window._gaq.push(['CATTGATC._trackEvent', 'InternalCampaign',  campaignID, document.location.host+document.location.pathname, 1, true]);
+        }
+
+        if (typeof cdpm.language != 'undefined') {
+            window._gaq.push(['CATTGATC._setCustomVar',  10, 'Language',  cdpm.language, 2]);
+        }
+        if (typeof cdpm.login != 'undefined') {
+            window._gaq.push(['CATTGATC._setCustomVar',  11, 'Login',  cdpm.login+"", 2]);
+        }
+
+        cdpm.utmaguid && window._gaq.push(['CATTGATC._setCustomVar',  30, 'UtmaGuid', cdpm.utmaguid, 1]);
+        cdpm.landingtimestamp && window._gaq.push(['CATTGATC._setCustomVar',  29, 'TimeStamp', cdpm.landingtimestamp, 2]);
+
+        if (cdpm.pageid == "") {
+            window._gaq.push(['CATTGATC._trackPageview']);
+        } else {
+            if ((/^flight$|^ssr$|^etf$|^oci$|^coupon$/i).test(cdpm.lob || '') || (/^flight$|^ssr$|^etf$|^oci$|^coupon$/i).test(cdpm.holidaytype || '')) {
+                window._gaq.push(['CATTGATC._trackPageview', fullVP.replace(/etf\/etf/, 'flight\/etf')])
+            } else     {
+                window._gaq.push(['CATTGATC._trackPageview']);
+            }
+        }
+
+        if(cdpm.errorcode){
+            var errorLabel= cdpm.errorcode+'&AppServer='+cdpm.appserver;
+            window._gaq.push(['CATTGATC._trackEvent', 'Errors', errorLabel, window.document.referrer, 1, true]);
+        }
+
+    } catch(e) {
+        cdl.error && cdl.error('GTM GATC:'+e)
+    }
+    else {window._gaq.push(['CATTGATC._trackPageview'])}
+    (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();
+
+    return cdl && cdpm
+}(window.CATTDL, !window.CATTDL?!1:window.CATTDL.CATTParams))
+</script>
