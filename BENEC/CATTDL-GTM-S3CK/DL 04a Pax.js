916c957e9358140bdd4ab34758f9a079a6580812
ttkyha <tranthikyha@gmail.com>
2015-04-03 00:52:34 +0100

    Update and rename DL 04 Pax.js to DL 04a Pax.js

--- BENEC/CATTDL-GTM-S3CK/DL 04a Pax.js
+++ BENEC/CATTDL-GTM-S3CK/DL 04a Pax.js
@@ -62,12 +62,13 @@
         cdpm['accomregion'] = ctpm.Region || '';
         cdpm['accomresort'] = ctpm.AccomResort || '';
         cdpm['deptdate'] = +(new Date(ctpm.DeptDate && ctpm.DeptDate.replace(/(\d+)\/(\d+)\/(\d+)/,'$3-$2-$1')) || new Date((jQuery('div#foCashTicket').find('div[class="flight-breakdown"]').find('span[class="trip-datetime"]').eq(0).text() || '').trim().replace(/(\d+)\/(\d+)\/(\d+)/,'$3-$2-$1').substring(0,10)) || '0');
-        cdpm['destairport'] = ctpm.DestinationAirportSelected || '';
-        cdpm['depairport'] = ctpm.DepartureAirportSelected || '';
+        cdpm['destairport'] = (ctpm.DestinationAirportSelected || '').toUpperCase();      
+        cdpm['depairport'] = (ctpm.DepartureAirportSelected || '').toUpperCase();
         cdpm['returndate'] = +(new Date(ctpm.RetDate && ctpm.RetDate.replace(/(\d+)\/(\d+)\/(\d+)/,'$3-$2-$1')) ||new Date((jQuery('div#foCashTicket').find('div[class="flight-breakdown"]').find('span[class="trip-datetime"]').eq(2).text() || '').trim().replace(/(\d+)\/(\d+)\/(\d+)/,'$3-$2-$1').substring(0,10)) || '0');
-        cdpm['duration'] = +(/\d+/.exec(jQuery('div#foCashTicket').find('div[class="flight-breakdown"]').find('span[class="trip-party"]').text()) || []).pop() || 0
+        cdpm['duration'] = +(/\d+/.exec(jQuery('div#foCashTicket').find('div[class="flight-breakdown"]').find('span[class="trip-party"]').text()) || []).pop() || 0;
         cdpm['roomcodes'] = ctpm.RoomType && ctpm.RoomType.split('|') || [];
         cdpm['sessionid'] = ctpm.SessId || '';
+        cdpm['starrating'] = ctpm.StarRating && ctpm.StarRating.replace(',','.');
         cdpm['pricetotal'] = (/\d+/.exec((jQuery('p.price.big-font') && jQuery('p.price.big-font').eq(0)).text() || '') || []).pop()
             || (/\d+/.exec((jQuery('div.price-total-line').find('[class="price"]').text() || '').replace('.','')) || []).pop()
         //cdpm['pricepp'] = (/\d+/.exec((jQuery('p.price.big-font') && jQuery('p.price.big-font').eq(1)).text() || '') || []).pop()
