d49ee3c23795b7bf246823d3a7a7c3a5cad97619
ttkyha <tranthikyha@gmail.com>
2015-04-03 00:10:44 +0100

    Update GATC 03 Accom.js

--- BENEC/CATTDL-GTM-S3CK/GATC 03 Accom.js
+++ BENEC/CATTDL-GTM-S3CK/GATC 03 Accom.js
@@ -6,7 +6,7 @@
             var cdpm = cdl.CATTParams || {};
 
             var CATTPageId = cdpm.pageid || "accom";
-            var VP = '/VP/'+(cdpm.lob || 'lob')+'/'+(cdpm.holidaytype || 'holidaytype')+'/'+CATTPageId);
+            var VP = '/VP/'+(cdpm.lob || 'lob')+'/'+(cdpm.holidaytype || 'holidaytype')+'/'+CATTPageId;
             var QSP_ST = 'SS_ST='+(cdpm.destination || '').replace(/\&amp;/g, '-').replace(/\&/g, '-')+'_'+(cdpm.accomname || '').replace(/\&amp;/g, '-').replace(/\&/g, '-')+'_'+(cdpm.accomcode || '');
             var QSP_CAT = 'SS_CAT='+(cdpm.lob || '')+'_'+CATTPageId;
             var QSP_P = 'DeptDate='+(cdl.gadate && cdpm.deptdate && cdl.gadate(cdpm.deptdate) || '').replace(/\//g, '-')
