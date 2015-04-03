4eef1d4006ff837a9999e43e74ac3e7c661cac64
ttkyha <tranthikyha@gmail.com>
2015-04-03 01:02:25 +0100

    Update DL 03 Accom.js

--- BENEC/CATTDL-GTM-S3CK/DL 03 Accom.js
+++ BENEC/CATTDL-GTM-S3CK/DL 03 Accom.js
@@ -62,8 +62,8 @@
         cdpm['accomregion'] = ctpm.Region || '';
         cdpm['accomresort'] = ctpm.AccomResort || '';
         cdpm['deptdate'] = +(new Date(ctpm.DeptDate && ctpm.DeptDate.replace(/(\d+)\/(\d+)\/(\d+)/,'$3-$2-$1'))) || 0;        
-        cdpm['destairport'] = ctpm.DestinationAirportSelected || '';
-        cdpm['depairport'] = ctpm.DepartureAirportSelected || '';
+        cdpm['destairport'] = (ctpm.DestinationAirportSelected || '').toUpperCase();      
+        cdpm['depairport'] = (ctpm.DepartureAirportSelected || '').toUpperCase();
         cdpm['duration'] = +(ctpm.duration || '0')
         cdpm['returndate'] = +(new Date(ctpm.RetDate && ctpm.RetDate.replace(/(\d+)\/(\d+)\/(\d+)/,'$3-$2-$1'))) || 0;
         cdpm['roomcodes'] = ctpm.RoomType && ctpm.RoomType.split('|') || [];
