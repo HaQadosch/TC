c01cd3df542343be1040f542f6a181f40d2068c9
ttkyha <tranthikyha@gmail.com>
2015-04-07 16:16:11 +0100

    Update DL UATC 01 LP.js

--- NLNEC_Angular/UA-GTM-5MRJ84/DL UATC 01 LP.js
+++ NLNEC_Angular/UA-GTM-5MRJ84/DL UATC 01 LP.js
@@ -14,7 +14,7 @@
             cookiedomain    : twlh(/neckermann.io/i)?"neckermann.io":"neckermann.nl",
             name            : 'CATTUATC',
             set             : {
-                anonymizeIp : false             
+                anonymizeIp : true             
             },
             webanalytics    : {
                 page            : loc && ((locpathname || '/')+(locsearch || '')) || '',
