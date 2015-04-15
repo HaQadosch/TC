2d9646671a8e8d27cfd40645b3a25f1a30c1b89a
ttkyha <tranthikyha@gmail.com>
2015-04-15 13:37:42 +0100

    Update DL UATC 08 Book.js

--- UKTC_Angular/GA-GTM-TZ9GH9/DL UATC 08 Book.js
+++ UKTC_Angular/GA-GTM-TZ9GH9/DL UATC 08 Book.js
@@ -12,9 +12,9 @@
         var cdpextr = cdpm.extras || {};           
 
         var params = JSON.parse(CATTDL.ckget('gtm_params') || '{}');
-        var srpsortlist = params.srplist || 'search';
-        var srpsortoption = params.sortoption || '';
-        var accomposition = params.accomposition[cdpm.accomguid || ''] || '';
+        var srpsortlist = params && params.srplist || 'search';
+        var srpsortoption = params && params.sortoption || '';
+        var accomposition = params && params.accomposition && params.accomposition[cdpm.accomguid || ''] || '';
 
         var vpagepath = '/vp/en/'+(cdpm.lob || 'angular')+'/'+(cdpm.holidaytype || 'angular')+'/'+(cdpm.pageid || 'angular');        
         var vpqsp_cat   = ('ss_cat='+ 
