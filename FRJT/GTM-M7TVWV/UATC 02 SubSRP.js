10da71a82d435934b728fb5e806c552b75fe5794
ttkyha <tranthikyha@gmail.com>
2015-04-16 19:44:07 +0100

    Create UATC 02 SubSRP.js

---
+++ FRJT/GTM-M7TVWV/UATC 02 SubSRP.js
@@ -0,0 +1,40 @@
+<script>
+(function gtm_uatcSubSRP(jQ, cdl, uadl, w, d, dl) {
+    'use strict';
+    if (cdl && uadl) try {
+        var trackerName = (uadl.name+".") || ""
+        var uawa = uadl.webanalytics || {}
+        var cdpm = CATTDL.CATTParams;
+
+        if(!w.ga) w = (function uatcGA(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m); return window})(w,d,'script','//www.google-analytics.com/analytics.js','ga')
+
+        window.ga && window.ga(function gtm_useTracker() {
+            var trc = ga.getByName(uadl.name)
+            if (!trc) {
+                w.ga('create', uadl.profileid, uadl.cookiedomain, {'name': uadl.name})
+                trc = ga.getByName(uadl.name)
+            }
+            for (var setOption in uadl.set) trc.set(setOption, uadl.set[setOption]);
+            if (typeof trc.plugins_ === 'undefined' || !/displayfeatures/i.test(trc.plugins_ && trc.plugins_.keys || '')) trc.require && trc.require('displayfeatures') || w.ga(trackerName+'require', 'displayfeatures');
+            cdl.CATTParams.gaguid =  /(.+)\./i.exec(trc.get('clientId') || '.').pop() || cdl.CATTParams.gaguid || ''
+            uawa && uawa.dimensions && (uawa.dimensions.dimension51 = {'gaguid' : cdl.CATTParams.gaguid || 'empty'}) || console.info('err', uawa)
+
+            var sendSet = {};
+            if (jQ){
+                jQ.each(uawa && uawa.dimensions || [], function gtm_keyDimension(kDim, vDim){
+                    jQ.each(vDim, function valDimension(_, val){val && (sendSet[kDim]=val)})
+                })
+                jQ.each(uawa && uawa.metrics || [], function gtm_keyMetrics(kMet, vMet){
+                    jQ.each(vMet, function valMetrics(_, val){val && (sendSet[kMet]=val)})
+                })  
+            }
+            sendSet['page'] = uawa.page;
+            trc.send('pageview', sendSet);
+        })
+        dataLayer.push({'event': 'UATC SubSRP'})
+    } catch(e) {
+        cdl.error('GTM FR JT UATC SubSRP: '+e)
+    }
+    return cdl && uadl
+}(window.jQuery, window.CATTDL, !window.CATTDL?!1:window.CATTDL.DL_uatc, window, document, window.dataLayer))
+</script>
