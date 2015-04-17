9aed43ce65eceba60c08d2ad2cafb52a4fc866e5
ttkyha <tranthikyha@gmail.com>
2015-04-17 11:13:57 +0100

    Update CATTDL_Core.js

--- UKTC_Angular/CATTDL-GTM-NB3GH9/CATTDL_Core.js
+++ UKTC_Angular/CATTDL-GTM-NB3GH9/CATTDL_Core.js
@@ -1,4 +1,4 @@
-//<script id='gtm_cattdlCore'>
+<script id='gtm_cattdlCore'>
 (function gtm_cattdlCore(jQ, d, w, dl){
     'use strict';
     var cdpm = '';
@@ -109,6 +109,13 @@
         cdpm.poolcontrol = !Boolean((cdpm.utmaguid || 1)%20);
 
          window.CATTDL.CATTParams = cdpm;
+
+        //UA call
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
     } catch(e) {
         var msg = 'GTM CATTDL Core: '+e;
         console && ((console.error)?console.error(msg):console.log(msg));
@@ -116,120 +123,4 @@
     dl.push({'event': 'core_'+(cdpm && cdpm.pageid || 'home')});
     return w.CATTDL;
 }(window.jQuery, window.document, window, window.dataLayer))
-//</script>
-
-
-//<script id='gtm_cattdlCore'>
-(function gtm_cattdlCore(jQ, d, w, dl){
-    'use strict';
-    var cdpm = '';
-    if (jQ) try{
-        w.CATTDL = w.CATTDL || {};
-        cdpm = w.CATTDL.CATTParams || {};
-        if(!String.prototype.trim){String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g,"")}}
-
-        w.CATTDL = {
-            twlh     : function twlh(rg) {return RegExp.prototype.test.call(rg, w.location.host)},
-            twdc     : function twdc(rg) {return RegExp.prototype.test.call(rg, document.cookie)},
-            ewdc     : function ewdc(rg) {return RegExp.prototype.exec.call(rg, document.cookie)},
-            twls     : function twls(rg) {return RegExp.prototype.test.call(rg, w.location.search)},
-            ewls     : function ewls(rg) {return RegExp.prototype.exec.call(rg, w.location.search)},
-            ckget     : function ckget(sKey) {return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null},
-            ckset     : function ckset(sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-                if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-                var sExpires = "";
-                if (vEnd) {
-                    switch (vEnd.constructor) {
-                        case Number:
-                            sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd; break;
-                        case String:
-                            sExpires = "; expires=" + vEnd; break;
-                        case Date:
-                            sExpires = "; expires=" + vEnd.toUTCString(); break;
-                    }
-                }
-                document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-                return true;
-            },
-            post     : function post(key, value) {document.cookie = key+"="+value+"; path=/; domain=.thomascook.com;"},
-            get     : function get(key) {
-                var regKey = new RegExp(key+'=([^;]*)', 'i');
-                return regKey.test(document.cookie) && (regKey.exec(document.cookie) || []).pop() || false;
-            },
-            info     : function info(msg) {console && ((console.info)?console.info(msg):console.log(msg)); return msg},
-            error     : function error(msg) {console && ((console.error)?console.error(msg):console.log(msg)); return msg}
-        };
-
-        w.CATTParams && jQ.each(w.CATTParams, function gtm_eachCATTParam(pP){
-            var pp = pP.toLowerCase();
-            var checked = "";
-            if (!cdpm[pp] || cdpm[pp] !== w.CATTParams[pP]){
-                checked = w.CATTParams[pP];
-                if (/string/i.test(typeof checked)) checked = checked.trim();
-                if (/lob|holidaytype/i.test(pp)) checked = checked.toLowerCase() || "generic";
-                else if (/pageid/i.test(pp)) checked = checked.toLowerCase() || "generic";
-                else if (/errorcode/i.test(pp)) checked = checked && checked.substr(0, 270) || "";
-                else if (/destination|boardbasis|searchresultstop3/i.test(pp)) checked = checked && checked.replace(/\&amp;/g, '-').replace(/\&/g, '-') || "";
-
-                if (checked) cdpm[pp] = checked;
-            }
-        });
-        cdpm.cookies = {}; (document.cookie.split(/;\s?/g)).forEach(function(coo, _){ var cur = /([^=]+)=(.*)/i.exec(coo); if (cur && cur.length > 1) cdpm.cookies[cur[1]]=cur[2]});
-        cdpm.searches = {}; (d.location.search.split(/\?|&/g).slice(1)).forEach(function(prm, _){ var cur = /([^=]+)=(.*)/i.exec(prm); if (cur && cur.length > 1) cdpm.searches[cur[1]]=cur[2]});
-
-        cdpm['internalcampaignid'] = cdpm.searches && cdpm.searches.intcmp
-            || w.CATTDL.twls(/intcmp=/i) && (w.CATTDL.ewls(/intcmp=([^&|\s|\?]*)/i) || []).pop()
-            || "";
-        cdpm['emailguid'] = cdpm.searches && cdpm.searches.user_id
-            || w.CATTDL.twls(/user_id=/i) && (w.CATTDL.ewls(/user_id=([^&]*)/i) || []).pop()
-            || "";
-        cdpm['ecircleid'] =  cdpm.searches && cdpm.searches.user_id
-            || w.CATTDL.twls(/user_id=/i) && (w.CATTDL.ewls(/user_id=([^&|\s|\?]*)/i) || []).pop()
-            || "";
-        cdpm['dreamcaptureid'] =  cdpm.searches && cdpm.searches.dc_id
-            || w.CATTDL.twls(/dc_id=/i) && (w.CATTDL.ewls(/dc_id=([^&|\s|\?]*)/i) || []).pop()
-            || "";
-        cdpm['abtanumber'] = cdpm.searches && cdpm.searches.abtaNumber
-            || w.CATTDL.twls(/abtaNumber=/i) && (w.CATTDL.ewls(/abtaNumber=([^&|\s|\?]*)/i) || []).pop()
-            || "";
-        cdpm['consultantref'] =  cdpm.searches && cdpm.searches.consultantRef
-            || w.CATTDL.twls(/consultantRef=/i) && (w.CATTDL.ewls(/consultantRef=([^&|\s|\?]*)/i) || []).pop()
-            || "";
-        cdpm['qsearchedkeyword'] = cdpm.searches && cdpm.searches.q
-            || w.CATTDL.ewls(/[&\?]q=([^&|\s|\?]*)/i) && w.CATTDL.ewls(/[&\?]q=([^&|\s|\?]*)/i).pop()
-            || "";
-
-        cdpm['utmaguid'] = cdpm.utmaguid
-            || cdpm.cookies.__utma && /^[^;\.%=]*\.([^;\.%=]*)\./i.exec(cdpm.cookies.__utma) && /^[^;\.%=]*\.([^;\.%=]*)\./i.exec(cdpm.cookies.__utma).pop()
-            || w.CATTDL.twdc(/_utma=/i) && w.CATTDL.ewdc(/_utma=[^;\.%=]*\.([^;\.%=]*)\./i) && w.CATTDL.ewdc(/_utma=[^;\.%=]*\.([^;\.%=]*)\./i).pop()
-            || "";
-        cdpm['gaguid'] = cdpm.gaguid
-            || cdpm.cookies && (/\.([^\.]*)\.\d*$/i.exec(cdpm.cookies._ga || '') || []).pop()
-            || w.CATTDL.twdc(/_ga=/i) && (w.CATTDL.ewdc(/_ga=[^\.]*\.\d+\.([^\.]*)\.\d+/i) || []).pop()
-            || "";
-        cdpm['utmbguid'] = cdpm.utmbguid
-            || cdpm.cookies.__utmb
-            || w.CATTDL.twdc(/_utmb=/i) && w.CATTDL.ewdc(/_utmb=([^;%=]*)/i) && w.CATTDL.ewdc(/_utmb=([^;%=]*)/i).pop()
-            || "";
-        cdpm['optimostcreative'] = window.opCreative
-            || "";
-
-        if (typeof w.CATTParams === 'undefined' && location.pathname === '/' || !cdpm.pageid ) cdpm.pageid = 'home';
-
-         cdpm['pagetimestamp'] = window.Date && window.Date.now();
-
-         cdpm['errors'] = cdpm.errorcode
-             || '';
-        cdpm.poolcontrol = !Boolean((cdpm.utmaguid || 1)%20);
-
-         window.CATTDL.CATTParams = cdpm;
-    } catch(e) {
-        var msg = 'GTM CATTDL Core: '+e;
-        console && ((console.error)?console.error(msg):console.log(msg))
-    } finally {
-        dl.push({'event': 'core_'+cdpm.pageid});
-    }
-
-    return w.CATTDL;
-}(window.jQuery, window.document, window, window.dataLayer, window.CATTDL))
 </script>
