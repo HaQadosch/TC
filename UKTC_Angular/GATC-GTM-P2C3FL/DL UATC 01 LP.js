a8b39dd83bd9cf0fa4bc43534f80e4a924d9a091
ttkyha <tranthikyha@gmail.com>
2015-04-22 17:52:27 +0100

    Create DL UATC 01 LP.js

---
+++ UKTC_Angular/GATC-GTM-P2C3FL/DL UATC 01 LP.js
@@ -0,0 +1,64 @@
+<script id='gtm_uatcdlLP'>
+(function gtm_uatcdlLP(cdl, dl, loc) {
+    'use strict';
+    if (cdl && cdl.CATTParams) try {
+        var twlh = cdl.twlh;
+        var cdpm = cdl.CATTParams;
+        var locpathname = loc.pathname || '';
+        var locsearch = loc.search || '';
+        var lochref = loc.href || document.URL || '';
+        var lochost = location.hostname || location.host || '';
+
+
+        if (!window.ga || window.ga.length < 1)  {
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m); return window})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        };
+
+        var myaccountactivation = /\/myaccount\/activate/.test(location.pathname)?'User_Activation':'';
+
+        cdl.DL_uatc = {
+            profileid       : 'UA-33036832-7',
+            cookiedomain    : twlh(/airtours/i)?"airtours.co.uk":(twlh(/club18-30/i)?"club18-30.com":(twlh(/directholidays/i)?"directholidays.co.uk":"thomascook.com")),
+            location        : loc && loc.href || '',
+            name            : 'CATTUATC',
+            set             : {
+                anonymizeIp : false
+            },
+            webanalytics    : {
+                page            : loc && ((locpathname || '/')+(locsearch || '')) || '',
+                location        : loc && loc.href || '',
+                metrics         : {
+                    metric15        : {'hitcount'       : 1}
+                },
+                dimensions      : {
+                    dimension6      : {'appserver'          : cdpm.appserver || ''},
+                    dimension12     : {'pageid'             : cdpm.pageid || ''},
+                    dimension19     : {'errorcode'          : cdpm.errors || ""},
+                    dimension30     : {'utmaguid'           : cdpm.utmaguid || ''},
+                    dimension31     : {'utmbguid'           : cdpm.utmbguid || ''},
+                    //dimension51     : {'gaguid'         : cdpm.gaguid || 'empty'},
+                    dimension32     : {'emailguid'          : cdpm.emailguid || ''},
+                    dimension52     : {'anonymousid'        : window.userId || ''},
+                    dimension53     : {'myaccountuserid'    : cdpm.user && cdpm.user.id || ''},
+                    dimension54     : {'myaccountaction'    : myaccountactivation || ''},
+                    dimension64     : {'dreamcaptureid'     : cdpm.dreamcaptureid || ''},
+                    dimension65     : {'pagetimestamp'      : cdl.gadate && cdl.gatime && cdl.gadate(cdpm.pagetimestamp || 0)+' '+cdl.gatime(cdpm.pagetimestamp || 0) || ''},
+                    dimension75     : {'unixtimestamp'      : ''+(cdpm.pagetimestamp || 0) || ''},
+                    dimension76     : {'tags'               : cdpm.poolcontrol && 'N' || 'Y'},
+                    dimension102    : {'pagecontext'        : cdpm.pagecontext || ''},
+                    dimension115    : {'errormessage'       : cdpm.errors && cdpm.errors.errormsg || ''}
+                },
+                events : {
+                internalcampaignid  : {'category': 'InternalCampaign', 'action': cdpm.internalcampaignid || '', 'label': lochost+locpathname, 'value': 1, 'noninteraction': true, 'page': locpathname+locsearch},
+                errorcode           : {'category': 'Errors', 'action': cdpm.errors || (cdpm.errors && cdpm.errors.Code) || '', 'label': lochost+locpathname+'/?destination='+(cdpm.destination || '').replace(/\&amp;/g, '-').replace(/\&/g, '-')+'&deptairport='+(cdpm.departureairportselected || cdpm.deptairport || '')+'&deptdate='+(cdpm.deptdate || '')+'&duration='+(cdpm.duration || '')+'&appserver='+(cdpm.appserver || '')+(((/[\?].+/.exec(lochref)) || '').toString()).replace(/\?/, '&'), 'value': 1, 'noninteraction': true, 'page': locpathname+locsearch}
+                }
+            }
+        };
+    } catch(e) {
+        cdl.error('GTM UK TC UATCDL LP: '+e);
+    } finally {
+        dl.push({'event': 'UATCDL LP'});
+    }
+    return cdl.DL_uatc;
+}(window.CATTDL, window.dataLayer || [], window.location))
+</script>
