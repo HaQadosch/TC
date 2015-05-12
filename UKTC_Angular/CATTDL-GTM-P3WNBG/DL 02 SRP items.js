75d428c2b2324de2e1e0971df5eaeedf2b7c253b
ttkyha <tranthikyha@gmail.com>
2015-05-12 17:07:26 +0100

    Update DL 02 SRP items.js

--- UKTC_Angular/CATTDL-GTM-P3WNBG/DL 02 SRP items.js
+++ UKTC_Angular/CATTDL-GTM-P3WNBG/DL 02 SRP items.js
@@ -53,8 +53,10 @@
             for (var i = 0; i < wgdItems.length; i++) {
                 curItem = wgdItems[i];
                 curpath = curItem.geoPath && curItem.geoPath.value && curItem.geoPath.value.split("/") || [];
-                curflightout = curItem.flights && curItem.flights[0] && curItem.flights[0].outbound || {};
-                curflightin = curItem.flights && curItem.flights[0] && curItem.flights[0].inbound || {};
+                curflightout = curItem.flights && curItem.flights[0] && curItem.flights[0].outbound && curItem.flights[0].outbound.legs || {};
+                curflightin = curItem.flights && curItem.flights[0] && curItem.flights[0].inbound && curItem.flights[0].inbound.legs || {};
+                var curflightoutlen = curflightout.length;
+                var curflightinlen = curflightin.length;
                 srpItems[i] = {
                     accomcode           :   (curItem.hotel && curItem.hotel.code || "").replace('|','-'),
                     accomguid           :   curItem.id || "",
@@ -65,40 +67,30 @@
                     accomregion         :   curpath.length > 1 && curpath[1] || "",
                     accomresort         :   curpath.length > 2 && curpath[2] || "",
                     accomconcept        :   curItem.concepts && curItem.concepts[0] && curItem.concepts[0].type || "",
-                    arrivalairportcode  :   curflightout && curflightout.legs && curflightout.legs[0] && curflightout.legs[0].arrival  && curflightout.legs[0].arrival.airportCode
-                                                ||curflightout && curflightout.legs[1] && curflightout.legs[1] && curflightout.legs[1].arrival  && curflightout.legs[1].arrival.airportCode //for 2 part journeys
+                    arrivalairportcode  :   curflightout && curflightout[curflightoutlen-1] && curflightout[curflightoutlen-1].arrival  && curflightout[curflightoutlen-1].arrival.airportCode
                                                 || "",
-                    arrivalairport      :   curflightout && curflightout.legs && curflightout.legs[0] && curflightout.legs[0].arrival  && curflightout.legs[0].arrival.title
-                                                ||curflightout && curflightout.legs[1] && curflightout.legs[1] && curflightout.legs[1].arrival  && curflightout.legs[1].arrival.title //for 2 part journeys
+                    arrivalairport      :   curflightout && curflightout[curflightoutlen-1] && curflightout[curflightoutlen-1].arrival  && curflightout[curflightoutlen-1].arrival.title
                                                 || "",
-                    arrivaltime         :   curflightout && curflightout.legs && curflightout.legs[0] && curflightout.legs[0].arrival  && curflightout.legs[0].arrival.time
-                                                ||curflightout && curflightout.legs[1] && curflightout.legs[1] && curflightout.legs[1].arrival  && curflightout.legs[1].arrival.time //for 2 part journeys
+                    arrivaltime         :   curflightout && curflightout[curflightoutlen-1] && curflightout[curflightoutlen-1].arrival  && curflightout[curflightoutlen-1].arrival.time
                                                 || 0,
                     boardbasis          :   curItem.priceDetail && curItem.priceDetail.boardTypeDesc || "",
-                    carrier             :   curflightout && curflightout.legs && curflightout.legs[0] && curflightout.legs[0].airlineCode
-                                                || curflightout && curflightout.legs && curflightout.legs[1] && curflightout.legs[1].airlineCode //for 2 part journeys
+                    carrier             :   curflightout && curflightout[0] && curflightout[0].airlineCode
                                                 || "",
-                    deptairportcode     :    curflightout && curflightout.legs && curflightout.legs[0] && curflightout.legs[0].departure && curflightout.legs[0].departure.airportCode
-                                                || curflightout && curflightout.legs && curflightout.legs[1] && curflightout.legs[1].departure && curflightout.legs[1].departure.airportCode //for 2 part journeys
+                    deptairportcode     :   curflightout && curflightout[0] && curflightout[0].departure && curflightout[0].departure.airportCode
                                                 || "",
-                    deptairport         :   curflightout && curflightout.legs && curflightout.legs[0] && curflightout.legs[0].departure && curflightout.legs[0].departure.title
-                                                || curflightout && curflightout.legs && curflightout.legs[1] && curflightout.legs[1].departure && curflightout.legs[1].departure.title //for 2 part journeys
+                    deptairport         :   curflightout && curflightout[0] && curflightout[0].departure && curflightout[0].departure.title
                                                 || "",
-                    deptdate            :   curflightout && curflightout.legs && curflightout.legs[0] && curflightout.legs[0].departure && curflightout.legs[0].departure.time
-                                                || curflightout && curflightout.legs && curflightout.legs[1] && curflightout.legs[1].departure && curflightout.legs[1].departure.time //for 2 part journeys
+                    deptdate            :   curflightout && curflightout[0] && curflightout[0].departure && curflightout[0].departure.time
                                                 || 0,
-                    depttime            :   curflightout && curflightout.legs && curflightout.legs[0] && curflightout.legs[0].departure && curflightout.legs[0].departure.time
-                                                || curflightout && curflightout.legs && curflightout.legs[1] && curflightout.legs[1].departure && curflightout.legs[1].departure.time //for 2 part journeys
+                    depttime            :   curflightout && curflightout[0] && curflightout[0].departure && curflightout[0].departure.time
                                                 || 0,
                     discountvalue       :   curItem.priceDetail && curItem.priceDetail.totalDiscount || "",
                     duration            :   curItem.duration  || "0",
                     pricepp             :   curItem.priceDetail && curItem.priceDetail.priceFrom || "",
                     pricetotal          :   curItem.priceDetail && curItem.priceDetail.amount || "",
-                    returndate          :   curflightin && curflightin .legs && curflightin .legs[0] && curflightin .legs[0].departure && curflightin .legs[0].departure.time
-                                                || curflightin && curflightin.legs && curflightin.legs[1] && curflightin.legs[1].departure && curflightin.legs[1].departure.time //for 2 part journeys
+                    returndate          :   curflightin && curflightin[curflightinlen-1] && curflightin[curflightinlen-1].departure && curflightin[curflightinlen-1].departure.time
                                                 || 0,
-                    returntime          :   curflightin && curflightin .legs && curflightin .legs[0] && curflightin .legs[0].departure && curflightin .legs[0].departure.time
-                                                || curflightin && curflightin.legs && curflightin.legs[1] && curflightin.legs[1].departure && curflightin.legs[1].departure.time //for 2 part journeys
+                    returntime          :   curflightin && curflightin[curflightinlen-1] && curflightin[curflightinlen-1].departure && curflightin[curflightinlen-1].departure.time
                                                 || 0,
                     starrating          :   curItem.ratings && curItem.ratings.starRating || "",
                     touroperator        :   curItem.tourOperator && curItem.tourOperator.name || ""
@@ -107,15 +99,14 @@
                 srpItems[i].flightdetails = {};
                 srpItems[i].flightdetails.outbound = [];
                 var curflightoutJ = {}
-                var fltoutcount = curflightout && curflightout.legs && curflightout.legs.length || 0;
-                for (var j = 0; j < fltoutcount; j++) {
-                    curflightoutJ = curflightout && curflightout.legs && curflightout.legs[j] || {};
+                for (var j = 0; j < curflightoutlen; j++) {
+                    curflightoutJ = curflightout && curflightout[j] || {};
                     srpItems[i].flightdetails.outbound.push({
                         depart  : {
                             date                : curflightoutJ.departure && curflightoutJ.departure.time && +(new Date(curflightoutJ.departure.time) || 0),
                             time                : curflightoutJ.departure && curflightoutJ.departure.time && +(new Date(curflightoutJ.departure.time) || 0),
                             airportcode         : curflightoutJ.departure && curflightoutJ.departure.airportCode || "",
-                            airport             : curflightoutJ.arrival && curflightoutJ.arrival.title,
+                            airport             : curflightoutJ.departure && curflightoutJ.departure.title,
                             carrier             : {
                                                     code :  curflightoutJ.airline && curflightoutJ.airlineCode || "",
                                                     name :  curflightoutJ.airline || ""
@@ -138,15 +129,14 @@
 
                 srpItems[i].flightdetails.inbound = [];             
                 var curflightinK = {}
-                var fltincount = curflightin && curflightin.legs && curflightin.legs.length || 0;
-                for (var k = 0; k < fltincount; k++) {
+                for (var k = 0; k < curflightinlen; k++) {
                     curflightinK = curflightin && curflightin.legs && curflightin.legs[k] || {}
                     srpItems[i].flightdetails.inbound.push({
                         depart  : {
                             date                : curflightinK.departure && curflightinK.departure.time && +(new Date(curflightinK.departure.time) || 0),
                             time                : curflightinK.departure && curflightinK.departure.time && +(new Date(curflightinK.departure.time) || 0),
                             airportcode         : curflightinK.departure && curflightinK.departure.airportCode || "",
-                            airport             : curflightinK.arrival && curflightinK.arrival.title,
+                            airport             : curflightinK.departure && curflightinK.departure.title,
                             carrier             : {
                                                     code :  curflightinK.airline && curflightinK.airlineCode || "",
                                                     name :  curflightinK.airline || ""
