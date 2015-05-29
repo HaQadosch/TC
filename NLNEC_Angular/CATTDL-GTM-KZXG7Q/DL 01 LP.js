48f5e0d7208f4c430cd3a104f9086d0b8d01b135
ttkyha <tranthikyha@gmail.com>
2015-05-29 17:26:36 +0100

    Update DL 01 LP.js

--- NLNEC_Angular/CATTDL-GTM-KZXG7Q/DL 01 LP.js
+++ NLNEC_Angular/CATTDL-GTM-KZXG7Q/DL 01 LP.js
@@ -1,24 +1,38 @@
-<script>
+<script id='gtm_cattdlLP'>
 (function gtm_cattdlLP(jQ, dl, cdl) {
-	'use strict'
-	if (jQ && jQ.extend && cdl) try {
-		var cdpm = cdl.CATTParams
-		var newPM = {}
-		var wgetData = window.getPageData && window.getPageData(cdpm.urlparams && cdpm.urlparams.pathname) || {}
+    'use strict'
+    if (jQ && jQ.extend && cdl) try {
+        var cdpm = cdl.CATTParams
+        var newPM = {}
+        var locpathname = cdpm.urlparams && cdpm.urlparams.pathname || location.pathname || '';
+        var wgetData = window.getPageData && window.getPageData(locpathname) || {}
 
-		cdpm.errors = {}
-		var errorPM = {};
-		errorPM['errorcode'] = wgetData.errorCode || "";
-		errorPM['errormsg'] = (wgetData.message || []).join(' ');
+        cdpm.errors = {}
+        var errorPM = {};
+        errorPM['errorcode'] = wgetData.errorCode || "";
+        errorPM['errormsg'] = (wgetData.message || []).join(' ');
 
-		jQ.extend(cdl.CATTParams.errors, errorPM);
-		window.CATTDL.CATTParams = cdpm;
-	} catch(e) {
-		cdl.error('GTM CATTDL LP: '+e)
-	} finally {
-		dl.push({'event': 'pid_'+cdl.CATTParams.pageid});
-		gatcDL.push({'event': 'CATTDL LP'})
-	}
-	return jQ && jQ.extend && cdl
+        var params = JSON.parse(CATTDL.ckget('gtm_params') || '{}');
+        params.initialholidaytype = (({
+                'vliegvakanties'    : 'package',
+                'autovakanties'     : 'carholidays',
+                'stedentrips'       : 'citytrips',
+                'wintersport'       : 'ski',
+                'lastminute'        : 'lastminute',
+                'kortevakanties'    : 'shortholidays',
+                ''                  : 'home'
+        })[(/\/([^\/]*)/i.exec(locpathname) || ['']).pop()]) || cdpm.initialholidaytype;
+        CATTDL.ckset('gtm_params', JSON.stringify(params), '', '/', '.neckermann.nl');
+        cdpm.initialholidaytype = params.initialholidaytype || '';
+
+        jQ.extend(cdl.CATTParams.errors, errorPM);
+        window.CATTDL.CATTParams = cdpm;
+    } catch(e) {
+        cdl.error('GTM CATTDL LP: '+e)
+    } finally {
+        dl.push({'event': 'pid_'+cdl.CATTParams.pageid});
+        gatcDL.push({'event': 'CATTDL LP'})
+    }
+    return jQ && jQ.extend && cdl
 }(window.jQuery, window.dataLayer, window.CATTDL))
 </script>
