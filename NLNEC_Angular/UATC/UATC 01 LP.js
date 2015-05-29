a73a675c4233d422cbaedf328fba24b834773837
ttkyha <tranthikyha@gmail.com>
2015-05-29 13:10:40 +0100

    Update UATC 01 LP.js

--- NLNEC_Angular/UATC/UATC 01 LP.js
+++ NLNEC_Angular/UATC/UATC 01 LP.js
@@ -4,6 +4,7 @@
     if (cdl && uadl) try{
         var trackerName = (uadl.name+".") || ""
         var uawa = uadl.webanalytics || {};
+        var cdpm = cdl.CATTParams || {};
 
         if (!window.ga) w = (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m); return window})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
