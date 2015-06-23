6520b8151633c29f4bd92fbb8883898797a67ee2
ttkyha <tranthikyha@gmail.com>
2015-06-23 19:02:21 +0100

    Update UATC 04 Accom.js

--- FRJT/GTM-M7TVWV/UATC 04 Accom.js
+++ FRJT/GTM-M7TVWV/UATC 04 Accom.js
@@ -1,26 +1,25 @@
-<script>
-(function gtm_uatcAccom(jQ, cdl, uadl, w, d, dl) {
+<script id='gtm_uatcSRP'>
+(function gtm_uatcSRP(jQ, cdl, uadl, w, d, dl) {
     'use strict';
     if (cdl && uadl) try {
         var trackerName = (uadl.name+".") || ""
         var uawa = uadl.webanalytics || {}
-        var uaprod = uawa.addproduct || {}
-        var cdpm = CATTDL.CATTParams || {}
+        var uaImp = uadl.webanalytics.addimpression || [];
+        var cdpm = CATTDL.CATTParams;
 
-        if(!w.ga) w = (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m); return window})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        if(!w.ga) w = (function uatcGA(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m); return window})(w,d,'script','//www.google-analytics.com/analytics.js','ga')
 
-        w.ga && w.ga(function gtm_useTracker() {
-            var trc = w.ga.getByName(uadl.name)
+        window.ga && window.ga(function gtm_useTracker() {
+            var trc = ga.getByName(uadl.name)
             if (!trc) {
                 w.ga('create', uadl.profileid, uadl.cookiedomain, {'name': uadl.name})
-                trc = w.ga.getByName(uadl.name)
+                trc = ga.getByName(uadl.name)
             }
-            for (var setOption in uadl.set) trc.set(setOption, uadl.set[setOption]);
-            trc.set('location', uawa.location);
+            for (var setOption in uadl.set) {trc.set(setOption, uadl.set[setOption])};
             if (typeof trc.plugins_ === 'undefined' || !/displayfeatures/i.test(trc.plugins_ && trc.plugins_.keys || '')) trc.require && trc.require('displayfeatures') || w.ga(trackerName+'require', 'displayfeatures');
             if (typeof trc.plugins_ === 'undefined' || !/ec/i.test(trc.plugins_ && trc.plugins_.keys || '')) trc.require && trc.require('ec', 'ec.js') || w.ga(trackerName+'require', 'ec', 'ec.js');
-            cdpm.gaguid =  /(.+)\./i.exec(trc.get('clientId') || '.').pop() || cdpm.gaguid || ''
-            uawa && uawa.dimensions && (uawa.dimensions.dimension51 = {'gaguid' : cdpm.gaguid || 'empty'}) || console.info('err', uawa)
+            cdl.CATTParams.gaguid =  /(.+)\./i.exec(trc.get('clientId') || '.').pop() || cdl.CATTParams.gaguid || ''
+            uawa && uawa.dimensions && (uawa.dimensions.dimension51 = {'gaguid' : cdl.CATTParams.gaguid || 'empty'}) || console.info('err', uawa)
 
             var sendSet = {};
             if (jQ){
@@ -30,63 +29,39 @@
                 jQ.each(uawa && uawa.metrics || [], function gtm_keyMetrics(kMet, vMet){
                     jQ.each(vMet, function valMetrics(_, val){val && (sendSet[kMet]=val)})
                 })  
-            }
-
-            if (cdpm.trailingsteps.replace(/.*accom\((\d)\).*$/g, '$1') === '1') {  
-                w.ga(trackerName+'ec:addProduct', {
-                    'id'        : uaprod.id,
-                    'name'      : uaprod.name,
-                    'category'  : uaprod.category,
-                    'brand'     : uaprod.brand,
-                    'variant'   : uaprod.variant,
-                    'list'      : uaprod.list,
-                    'position'  : uaprod.position
-                });
-
-                w.ga(trackerName+'ec:setAction', 'click', {'list': uaprod.list});
-                w.ga(trackerName+'send','event', 'ECProductListClick', uaprod.id,  ''+uaprod.position, 1, {'nonInteraction': true});
-                dl.push({'event': 'UATC EC Accom'})
             };
-
-            var interactionCounter = 1;
-            jQ('a.voir[id!=link-availibility]').on('click', function gtm_clickAddProduct(e){
-
-                var onclk = jQ(e.target).attr('onclick') || '';
-                onclk = onclk && onclk.split(/\('|'\)/g) || '';
-                onclk = onclk && onclk.length > 2 && parseInt(onclk[1]) || ''
-
-                var resa = w.tagManageribe && w.tagManageribe.length && w.tagManageribe[0] || ''
-                resa = resa && resa.offerList && resa.offerList.length > onclk && resa.offerList || ''
-                resa = (resa && resa.filter(function(e){return typeof e.orderDisplay !== 'undefined' && e.orderDisplay === onclk}) || ['']).pop()
-    
-                var variant = ((cdpm.boardbasis || "")+'|'+
-                (resa && resa.offerDepartureAirport && resa.offerDepartureAirport.replace(/^([^;]+).*/, '$1') || '')+'|'+
-                (resa && resa.offerDate || '')+'|'+
-                (cdl.gadate && cdl.gadate(cdpm.retdate || 0) || "")+'|'+
-                (cdpm.paxtotal || "0")+'|'+
-                (cdpm.paxadult || cdpm.srchnbradults || "0")+'|'+
-                (cdpm.paxchild || "0")+'|'+
-                (cdpm.paxinfant || "0")+'|'+
-                (resa && resa.offerBestPrice || "0")).toLowerCase()
-
-                w.ga(trackerName+'ec:addProduct', {
-                    'id': uaprod.id,
-                    'name': uaprod.name,
-                    'category': uaprod.category,
-                    'brand': (resa && resa.offerTourOperator || '').toLowerCase(),
-                    'variant': variant
-                });
-                w.ga(trackerName+'ec:setAction', 'detail');
-                trc.send('event', 'ECProductView', uaprod.id, ''+(interactionCounter++));
-                dl.push({'event': 'UATCECaddProduct'});
-            })
-
+            
             sendSet['page'] = uawa.page;
-            trc.send('pageview', sendSet);
+            w.ga(trackerName+'send','pageview', sendSet);
+
+            (function gtm_uatcSRPAddImpressions(){
+                var curUAImp = {}
+                if (uawa.nbrimpressions && uawa.addimpression && uawa.addimpression.length){
+                    for (var i = 0; i<uawa.nbrimpressions; i++){
+                        curUAImp = uaImp[i] || {}
+                        w.ga(trackerName+'ec:addImpression', {
+                                  'id':             curUAImp.id || '',
+                                  'name':           curUAImp.name || '',
+                                  'category':       curUAImp.category || '',
+                                  'brand':          curUAImp.brand || '',
+                                  'variant':        curUAImp.variant || '',
+                                  'list':           curUAImp.list || '',
+                                  'position':       curUAImp.position || '',
+                                  'dimension27':    curUAImp.dimension27 || ''
+                        })
+                    };
+                }
+            }());
+            var accoms = [];
+            for (i = 0; i < uawa.nbrimpressions; accoms.push((uaImp[i++].id) || ""));
+            w.ga(trackerName+'ec:setAction', 'view', {'list': (uaImp[0] && uaImp[0].list || "")});      
+            w.ga(trackerName+'send','event', 'viewAddImpression', (uaImp[0].list || ""),  (accoms && accoms.toString() || ""), 1, {'nonInteraction': true, 'location': uawa.location});
+
+            dl.push({'event': 'UATCECaddImpression'});
         })
-        dataLayer.push({'event': 'UATC Accom'})
+        dataLayer.push({'event': 'UATC SRP'})
     } catch(e) {
-        cdl.error('GTM FR JT UATC Accom: '+e);
+        cdl.error('GTM FR JT UATC SRP: '+e)
     }
     return cdl && uadl
 }(window.jQuery, window.CATTDL, !window.CATTDL?!1:window.CATTDL.DL_uatc, window, document, window.dataLayer))
