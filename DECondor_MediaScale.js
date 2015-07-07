897b6f7a87f058d25caab8de302cca5c22a828dc
HaQadosch <haqadosch@gmail.com>
2015-07-07 12:22:47 +0100

    DE condor mediascale flight test

--- DECondor_MediaScale.js
+++ DECondor_MediaScale.js
@@ -1,44 +1,36 @@
-function transpose(s, d){
-  for (var p in d) s = s.replace(new RegExp('{'+p+'}', 'g'), d[p]);
-  return s;
-}
-
-var build = function build(id, href){
-  var options = {
-    id: id,
-    href: href
-  };
-  return t('<div id="tab"><a href="{href}" id="{id}" /></div>', options);
-}
-
-(function gtm_mediascale(jQ, cdl, cdpm){
+//<script id='gtm_mediascale'>
+(function gtm_mediascale(jQ, cdl, cdpm, dl){
     'use strict';
-    if (jQ && cdl && cdpm) try {
+    if (jQ && cdl && cdpm && dl && cdl.twdc(/test=mediascale/)) try {
         var options = {
-            'depairport' : cdpm.departureairportselected || '',
-            'destairport' : cdpm.destinationairportselected || '',
-            'depdate' : cdpm.deptdateselectedMonth_2DigitYear || '',
-            'comp' : cdpm.compartment || '',
+            'depairport' : cdpm.departureairportsearched || '',
+            'destairport' : cdpm.destinationairportsearched || '',
+            'depdate' : cdpm.deptdate.replace(/\d\d(\d\d)-(\d\d)-\d\d/, '$2$1') || '',
+            'comp' : cdpm.compartment || ''
         };
-        cdl.assign && cdl.assign(cdpm, options, {
-            'start' : new Date().getTime(),
-            'pixel' : {
-                'src' : 'https://ad3.adfarm1.adition.com/tagging?network=250&type=src&tag[condor.page]=search&tag[condor.data]=<DepartureAirportSelected>,<DestinationAirportSelected>,<DeptDateSelected-Month><DeptDateSelected-2DigitYear>,<Compartment>'
-           }
-        });
+        var dlMediaScale = {
+           'DL_mediascale' : cdl.assign({
+               'start' : new Date().getTime(),
+               'img' : {
+                   'src' : cdl.transpose && cdl.transpose('https://ad3.adfarm1.adition.com/tagging?network=250&type=src&tag[condor.page]=search&tag[condor.data]={depairport},{destairport},{depdate},{comp}&source=gtm', options) || ''
+               }
+           }, options)
+       };
+       cdl.assign && cdl.assign(cdl, dlMediaScale);
+       cdl.assign && dl.push && jQ().append &&  jQ.when && jQ.when(jQ('body').append(jQ('<img>', {
+           src : cdl.DL_mediascale && cdl.DL_mediascale.img && cdl.DL_mediascale.img.src || '',
+           style : 'border-style:none;',
+           alt : '',
+           id : 'gtm_mediaScalePixel',
+           width : 1,
+           height : 1
+       }))).done(function gtm_mediaScaleDone() {dl.push(cdl.assign({}, {'event': 'mediascale'}, dlMediaScale))});
     } catch(e) {
-
-    } finally {
-
+        cdl.error && cdl.error('GTM mediascale : '+e);
     }
-    return jQ && cdl;
-}(window.jQuery, window.CATTDL, !window.CATTDL?!1:window.CATTDL.CATTParams));
-
-
-var img2 = document.createElement('img'); // use DOM HTMLImageElement
-img2.src = 'image2.jpg';
-img2.alt = 'alt text';
-document.body.appendChild(img2);
+    return jQ && cdl && cdpm;
+}(window.jQuery, window.CATTDL, !window.CATTDL?!1:window.CATTDL.CATTParams, window.dataLayer));
+//</script>
 
 URL = https://www.condor.com/tcibe/de/flight/flights
 PageID = select
