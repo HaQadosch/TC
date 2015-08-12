dd6dc62643a7c6c91a2c830fdf9af5bd3a8857ca
ttkyha <tranthikyha@gmail.com>
2015-08-12 13:29:44 +0100

    Update My Account Login.js

--- UKTC_Angular/GA-GTM-TZ9GH9/My Account Login.js
+++ UKTC_Angular/GA-GTM-TZ9GH9/My Account Login.js
@@ -1,44 +1,68 @@
-<script>
+<script id='gtm_uamyaccountLogin'>
 (function gtm_uamyaccountLogin(jQ, cdl, uadl, w){
-		'use strict';
-	if (cdl && uadl) try{	
+        'use strict';
+    if (cdl && uadl) try{   
 
-	var cdpm = cdl.CATTParams || [];
-	var trackerName = (uadl.name+".") || "";
-	var uawa = uadl.webanalytics || {};
+    var cdpm = cdl.CATTParams || [];
+    var trackerName = (uadl.name+".") || "";
+    var uawa = uadl.webanalytics || {};
+    var wgD = window.getPageData && window.getPageData('user') || {}
 
-	//var myaccountid = (w.getPageData('user') && w.getPageData('user').data.id) || (w.getPageData('user') && w.getPageData('user').data.data) || "";
-	
+    //var myaccountid = (w.getPageData('user') && w.getPageData('user').data.id) || (w.getPageData('user') && w.getPageData('user').data.data) || "";
+    var loginerror = jQ('div#login-signup-form div.alert-box.alert.ng-binding.alert-danger').next().text()
+      ||  'Uncaptured Error';
+
     w.ga(trackerName+'send', 'event'
-			, 'MyAccountLogin'
-			, (cdpm.user && cdpm.user.action || "")
-			, (cdpm.user && cdpm.user.msg || "")
-			, 1
-			, { 'dimension51': cdpm.gaguid || 'empty',
-			    'dimension65': cdl && cdl.gadate && cdl.gatime && window.Date && cdl.gadate(window.Date.now())+' '+cdl.gatime(window.Date.now()) || '',
-			    'dimension75': ''+(window.Date && window.Date.now() || 0)}                          
-			, {'nonInteraction': true});
-	}
-	catch(e){cdl.error('GTM UATC MyAccountLogin: '+e)}
+            , 'MyAccountLogin'
+            , (cdpm.user && cdpm.user.action || "")
+            , (cdpm.user && cdpm.user.msg || loginerror || "")
+            , 1
+            , { 'dimension51': cdpm.gaguid || ''
+                ,'dimension52': window.userId || '' //my account anonymous id
+                ,'dimension53': (cdpm.user && cdpm.user.msg || "") || '' //my account userid
+                ,'dimension54': (cdpm.user && cdpm.user.action || "") || '' // action
+                ,'dimension65': cdl && cdl.gadate && cdl.gatime && window.Date && cdl.gadate(window.Date.now())+' '+cdl.gatime(window.Date.now()) || ''
+                ,'dimension75': ''+(window.Date && window.Date.now() || 0)}                          
+            , {'nonInteraction': true});
+    }
+    catch(e){cdl.error('GTM UATC MyAccountLogin: '+e)}
 }(!window.jQuery?!1:window.jQuery, !window.CATTDL?!1:window.CATTDL, !window.CATTDL.DL_uatc?!1:window.CATTDL.DL_uatc, window))
 </script>
 
 <script>
 (function gtm_gamyaccountLogin(jQ, cdl, gadl, w){
-		'use strict';
-	if (cdl && gadl) try{	
+        'use strict';
+    if (cdl && gadl) try{   
 
-	var cdpm = cdl.CATTParams || [];
-	var trackerName = (gadl.trackername+".") || "";
-	var uawa = uadl.webanalytics || {};
+    var cdpm = cdl.CATTParams || [];
+    var trackerName = (gadl.trackername+".") || "";
+    var wgD = window.getPageData && window.getPageData('user') || {}    
+    var loginerror = jQ('div#login-signup-form div.alert-box.alert.ng-binding.alert-danger').next().text() 
+      ||  'Uncaptured Error';
 
+    if(cdpm.user && cdpm.user.msg){
+    w._gaq.push([trackerName+'_setCustomVar'
+              , 33                 
+              , 'MyAccountUserID'      
+              , cdpm.user.msg
+              , 3                    
+             ]);
+    }   
+    if(window.userId){
+    w._gaq.push([trackerName+'_setCustomVar'
+              , 34                   
+              , 'MyAccountAnonymousID'      
+              , window.userId
+              , 3                    
+             ]);
+    }
     w._gaq.push([trackerName+'_trackEvent'
             , 'MyAccountLogin'
             , (cdpm.user && cdpm.user.action || "")
-            , (cdpm.user && cdpm.user.msg || "")
+            , (cdpm.user && cdpm.user.msg || loginerror || "")
             , 1     
             , {'noninteraction': true}]);  
-	}
-	catch(e){cdl.error('GTM GATC MyAccountLogin: '+e)}
+    }
+    catch(e){cdl.error('GTM GATC MyAccountLogin: '+e)}
 }(!window.jQuery?!1:window.jQuery, !window.CATTDL?!1:window.CATTDL, !window.CATTDL.DL_gatc?!1:window.CATTDL.DL_gatc, window))
 </script>
