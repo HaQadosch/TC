010c2b2ed240a51030f3173cb00454f87df90e26
Xavier Brinon <xavier.brinon@thomascookonline.com>
2015-09-14 16:56:13 +0100

    UK TC Multicom Update

--- GTM-KH4KVH/DL CATT 2 SRP.js
+++ GTM-KH4KVH/DL CATT 2 SRP.js
@@ -1,60 +1,59 @@
 <script id='GTM-KH4KVH_cattdlSRP'>
     (function gtm_cattdlSRP(jQ, dl, cdl) {
-        'use strict';
-        if (jQ && jQ.extend && cdl && !dl.some(function(a){return /CATTDL SRP/i.test(a && a.event || '');}) ) try {
-            var cdpm = cdl.CATTParams;
-            var newPM = {};
-            var keeps = {};
+      'use strict';
+      var timeStart = cdl.CATTParams?cdl.CATTParams.timestart:0;
+      var cdpm;
+      var ld = cdl._;
+      if (jQ && cdl && !dl.some(function(a){return /CATTDL SRP/i.test(a && a.event || '');}) ) try {
+        cdpm = cdl.CATTParams;
+        var keeps = {};
+        var ck_cdpm = JSON.parse(cdl.ckget('gtm_cdpm') || '{}');
 
-            var ck_cdpm = JSON.parse(cdl.ckget('gtm_cdpm') || '{}');
+        var jqmain = jQ('div#fh_results');
+        var jqSelPage =  jqmain.find('div.numbers em span:eq(0)').text() || '1';
+        var jqSortOption = jqmain.find('select.ordered-dropdown > option:selected').text()  || "";
+        var accoms = jqmain.find('div.ResultsListView').toArray().map(function (e, i) {
+          return {
+            'name'      : jQ(e).find('span.headerInfo span.name').text()
+            , 'id'        : cdpm.accomguid || cdpm.accomcode || ''
+            , 'totalprice': (/(\d+.?\d+)/.exec(jQ(e).find('.ResultItemHeaderTotalPrice').text() || '') || []).pop() || ''
+            , 'pricepp'   : (/(\d+.?\d+)/.exec(jQ(e).find('.ResultItemHeaderPricePerPerson').text() || '') || []).pop() || ''
+            , 'location'  : jQ(e).find('.location').text() || ''
+            , 'starrating': (/(\d+)/i.exec(jQ(e).find('span.starRating img').attr('title') || '') || []).pop() || ''
+            , 'duration'  : cdpm.duration || ''
+            , 'boardbasis': (jQ(e).find('.boardBasisOptions .option.selected input').val() || '').toLowerCase()
+            , 'imgurl'    : jQ(e).find('div.ResultItemInfoImage img').attr('src') || ''
+            , 'resort'    : jQ(e).find('.location').text() || ''
+            , 'pos'       : i
+          };
+        });
 
-            newPM.deptdate = cdpm.deptdate || (/dd=(\d\d)(\d\d)(\d\d\d\d)/i.exec((jQ('a[href*=accomIndex]:eq(0)').attr('href') || '')) || []).slice(1).join('/').replace(/(\d\d)\/(\d\d)\/(\d\d\d\d)/g, '$2/$1/$3') || '';
-            newPM.selpage = jQ('.pager.alt span.active:eq(0)').text() || cdpm.searches.pageNum || cdpm.searchresultspagenbr || jQ(jQ('div.numbers em span')[1]).text() || 1;
-            newPM.sortoption = jQ('#SelectOrderType > option:selected').text().replace(/\(|\)/g, '')  || jQ('select#sortby :selected').val() || (jQ('select.ordered-dropdown option[selected="selected"]').text()) || cdpm.sortoption || "";
+        var departweeknbr = (function gtm_weekNbr(dd){
+          var wNbr = 0;
+          var date = new Date(dd);
+          var dateY = date.getFullYear();
+          var firstOfJan = new Date(dateY,0,1);
+          wNbr = Math.ceil((((date - firstOfJan) / 86400000) + firstOfJan.getDay()+1)/7) || 0;
+          return wNbr || 0;
+        }(cdpm.deptdate || ''));
 
-            var accoms = jQ('div.ResultsListView');
-            newPM.accoms = [];
+        keeps = {
+          'accoms': accoms
+          , 'departweeknbr': departweeknbr
+          , 'selpage': jqSelPage
+          , 'sortoption': jqSortOption
+        };
 
-            if (accoms.length) {
-                accoms.each(function gtm_forEachAccoms(i, e) {
-                    newPM.accoms.push({
-                        'name' 			 : jQ(e).find('.name').text() || '',
-                        'id' 			   : (/(\d+)/i.exec(jQ(e).find('.exactMatch').attr('id')) || []).pop() || '',
-                        'totalprice' : (/(\d+.?\d+)/.exec(jQ(e).find('.ResultItemHeaderTotalPrice').text() || '') || []).pop() || '',
-                        'pricepp'    : (/(\d+.?\d+)/.exec(jQ(e).find('.ResultItemHeaderPricePerPerson').text() || '') || []).pop() || '',
-                        'location'   : jQ(e).find('.location').text() || '',
-                        'starrating' : (/(\d+)/i.exec(jQ(e).find('.starRating').attr('title') || '') || []).pop() || '',
-                        'duration' 	 : cdpm.duration || '',
-                        'boardbasis' : (jQ(e).find('.boardBasisOptions .option.selected input').val() || '').toLowerCase(),
-                        'imgurl' 		 : jQ(e).find('.ResultItemInfoHotelImage img').attr('src') || '',
-                        'resort' 		 : jQ(e).find('.location').text() || '',
-                        'position' 	 : i+1  || 0
-                    });
-                });
-            }
+        ld.assign(cdl.CATTParams, keeps);
+        ld.assign(ck_cdpm, keeps);
 
-            keeps.departweeknbr = (function gtm_weekNbr(dd){
-                var wNbr = 0;
-                var dates = dd && dd.split(/\//g) || [];
-                if (dates.length > 2){
-                    var dateY = dates.pop();
-                    var date = new Date(dateY, dates.pop()-1, dates.pop());
-                    var firstOfJan = new Date(dateY,0,1);
-                    wNbr = Math.ceil((((date - firstOfJan) / 86400000) + firstOfJan.getDay()+1)/7) || 0;
-                }
-                return wNbr || 0;
-            }(newPM.deptdate || ''));
-
-            jQ.extend(cdl.CATTParams, newPM, keeps);
-            jQ.extend(ck_cdpm, keeps);
-            var domainName = cdl.twlh(/airtours/i)?'.airtours.co.uk':(cdl.twlh(/club18-30/i)?'.club18-30.com':(cdl.twlh(/directholidays/gi)?'.directholidays.co.uk':'.thomascook.com'));
-            cdl.ckset('gtm_cdpm', JSON.stringify(keeps), Infinity, '/', domainName);
-        } catch(e) {
-            cdl.error('GTM CATTDL SRP: '+e);
-        } finally {
-            var stampEpoch = +new Date();
-            dl.push({'event': 'CATTDL SRP', 'pid': cdl.CATTParams.pageid, 'timestamp': stampEpoch, 'since gtm.start': stampEpoch - window.dataLayer[0]['gtm.start']});
-        }
-        return jQ && jQ.extend && cdl;
+        cdl.ckset('gtm_cdpm', JSON.stringify(keeps), Infinity, '/', '.thomascook.com');
+      } catch(e) {
+        cdl.error('GTM CATTDL SRP: '+e);
+      } finally {
+        var stampEpoch = +new Date();
+        dl.push({'event': 'CATTDL SRP', 'pid': cdl.CATTParams.pageid, 'timestamp': stampEpoch, 'since gtm.start': stampEpoch - timeStart});
+      }
+      return jQ && cdl;
     }(window.jQuery, window.dataLayer, window.CATTDL));
 </script>
--- GTM-KH4KVH/DL CATT 4 PaxPay.js
+++ GTM-KH4KVH/DL CATT 4 PaxPay.js
@@ -1,39 +1,36 @@
 <script id='GTM-KH4KVH_cattdlpaxpay'>
     (function gtm_cattdlPaxPay(jQ, dl, cdl){
-        'use strict';
-        if (jQ && jQ.extend && cdl && !dl.some(function(a){return /CATTDL PaxPay/i.test(a && a.event || '');}) ) try {
-            var newPM = {};
+    'use strict';
+    var timeStart = cdl.CATTParams?cdl.CATTParams.timestart:0;
+    var ld = cdl._;
+        if (jQ && cdl && !dl.some(function(a){return /CATTDL PaxPay/i.test(a && a.event || '');}) ) try {
             var keeps = {};
-
             var ck_cdpm = JSON.parse(cdl.ckget('gtm_cdpm') || '{}');
-            var domainName = cdl.twlh(/airtours/i)?'.airtours.co.uk':(cdl.twlh(/club18-30/i)?'.club18-30.com':(cdl.twlh(/directholidays/gi)?'.directholidays.co.uk':'.thomascook.com'));
-
-            newPM.roomtype = jQ('.room-type').next().text() || '';
 
             var jqpax = jQ('div.PassengerDetails');
             jqpax.find('input[name="pax_inputs_fields:leadPaxPanel:firstname"]').on('blur', function(e){
                 var fn = jQ(e.currentTarget).val() || '';
                 keeps.firstname = fn;
-                jQ.extend(cdl.CATTParams, newPM, keeps);
-                cdl.ckset('gtm_cdpm', JSON.stringify(keeps), Infinity, '/', domainName);
+                ld.assign(cdl.CATTParams, ck_cdpm, keeps);
+                cdl.ckset('gtm_cdpm', JSON.stringify(keeps), null, '/', '.thomascook.com');
             });
 
             jqpax.find('input[name="pax_inputs_fields:leadPaxPanel:surname"]').on('blur', function(e){
                 var ln = jQ(e.currentTarget).val() || '';
                 keeps.lastname = ln;
-                jQ.extend(cdl.CATTParams, newPM, keeps);
-                cdl.ckset('gtm_cdpm', JSON.stringify(keeps), Infinity, '/', domainName);
+                ld.assign(cdl.CATTParams, ck_cdpm, keeps);
+                cdl.ckset('gtm_cdpm', JSON.stringify(keeps), null, '/', '.thomascook.com');
             });
 
-            jQ.extend(cdl.CATTParams, newPM, keeps);
-            jQ.extend(ck_cdpm, keeps);
-            cdl.ckset('gtm_cdpm', JSON.stringify(keeps), Infinity, '/', domainName);
+            ld.assign(cdl.CATTParams, ck_cdpm, keeps);
+            ld.assign(ck_cdpm, keeps);
+            cdl.ckset('gtm_cdpm', JSON.stringify(keeps), null, '/', '.thomascook.com');
 
         } catch(e) {
             cdl.error('GTM CATTDL PaxPay: '+e);
         } finally {
             var stampEpoch = +new Date();
-            dl.push({'event': 'CATTDL PaxPay', 'pid': cdl.CATTParams.pageid, 'timestamp': stampEpoch, 'since gtm.start': stampEpoch - window.dataLayer[0]['gtm.start']});
+          dl.push({'event': 'CATTDL PaxPay', 'pid': cdl.CATTParams.pageid, 'timestamp': stampEpoch, 'since gtm.start': stampEpoch - timeStart});
         }
         return jQ && jQ.extend && cdl;
     }(window.jQuery, window.dataLayer, window.CATTDL));
