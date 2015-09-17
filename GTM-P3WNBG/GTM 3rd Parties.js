123b09c4004d45df811c1c59e557a2676b9021b0
Xavier Brinon <xavier.brinon@thomascookonline.com>
2015-09-17 15:33:00 +0100

    UK TC Angular 3rd party tag
    First import

---
+++ GTM-P3WNBG/GTM 3rd Parties.js
@@ -0,0 +1,15 @@
+<script id='gtm_3rdParty'>
+(function gtm_3rdParty(w,d,s,l,i){
+    var cdpm = window.CATTDL && window.CATTDL.CATTParams || '';
+
+    if ((cdpm && cdpm.cookies && cdpm.cookies.gtm_tags || '')?/variant/i.test(cdpm.cookies.gtm_tags):!cdpm.poolcontrol) {
+        if (window.google_tag_manager && !window.google_tag_manager['GTM-M8NMSC']){
+            w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            window.dataLayer && window.dataLayer.push({'event': 'gtm 3rd party'});
+        }
+    }
+    return window.dataLayer && window.dataLayer.push({'event':'3rd Party'}) || [];
+})(window,document,'script','dataLayer_M8NMSC','GTM-M8NMSC');
+</script>
