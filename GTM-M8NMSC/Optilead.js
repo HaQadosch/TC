7a9e1a9f772f588d0f10548f3cc7ca8f202a1b30
Xavier Brinon <xavier.brinon@thomascookonline.com>
2015-09-30 14:31:39 +0100

    Optilead removed GATC pool tracking

---
+++ GTM-M8NMSC/Optilead.js
@@ -0,0 +1,16 @@
+<script id='GTM-M8NMSC_optilead'>
+  (function gtm3rd_Optilead(cdl, dl){
+    'use strict';
+    var oldl = {};
+    if (dl && cdl) try {
+      oldl = cdl.insertJS({
+          'name': 'optilead'
+        , 'src': 'https://analytics.optilead.co.uk/webscrape/tc2.js'
+        , 'dataLayer' : window.dataLayer_M8NMSC
+      });
+    } catch(e) {
+      cdl.error && cdl.error('GTM Optilead: '+e);
+    }
+    return dl && cdl && oldl;
+  }(window.CATTDL, window.dataLayer_M8NMSC));
+</script>
